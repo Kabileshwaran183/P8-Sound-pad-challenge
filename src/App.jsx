8d2160d4b7f7169b65a3f9bd3af905abd682386f
Kabileshwaran183 <Kabileshwaran183@gmail.com>
2024-12-18 06:42:44 +0530

    updating the array by setting toggle function in the parent component

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,14 @@ export default function App() {
     const [pads, setPads] = React.useState(PadsData)
 
     const buttonElements = pads.map(pad => (
-        <Pad key={pad.id} color={pad.color} on={pad.on}/>
+        <Pad key={pad.id} id={pad.id} toggle={toggle} color={pad.color} on={pad.on}/>
     ))
+
+    function toggle(id){
+        setPads(prevPads=>prevPads.map(item=>{
+            return item.id === id ? {...item,on:!item.on} : item
+        }))
+    }
     
     return (
         <main>
