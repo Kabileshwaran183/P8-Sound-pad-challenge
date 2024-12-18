a6590fa08e6cdfd1d689cfdc5196ac9bfcae5884
Kabileshwaran183 <Kabileshwaran183@gmail.com>
2024-12-18 07:11:59 +0530

    TurnON TurnOff Button

--- src/App.jsx
+++ src/App.jsx
@@ -14,11 +14,29 @@ export default function App() {
             return item.id === id ? {...item,on:!item.on} : item
         }))
     }
+    function turnAllPadsOff() {
+        console.log("Turning off")
+        setPads(prevPads => prevPads.map(pad => ({
+            ...pad,
+            on: false
+        })))
+    }
+    function turnAllPadsOn() {
+        console.log("Turning off")
+        setPads(prevPads => prevPads.map(pad => ({
+            ...pad,
+            on: true
+        })))
+    }
     
     return (
         <main>
             <div className="pad-container">
                 {buttonElements}
+            </div>
+            <div className="all-button">
+            <button className="all-off" onClick={turnAllPadsOff}>Turn All Off</button>
+            <button className="all-on" onClick={turnAllPadsOn}>Turn All On</button>
             </div>
         </main>
     )
